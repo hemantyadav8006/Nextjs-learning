b5a5f9c3f001a650da37c4f626c719c56ab9988d
Hemant Kumar Yadav <55074926+hemantyadav8006@users.noreply.github.com>
2025-01-14 13:09:03 +0530

    add Services, Dashboard, and API pages routing

---
+++ app/Servicess/page.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const ServicesPage: NextPage = () => {
+  return (
+    <div>
+      <h1>Services Page</h1>
+    </div>
+  );
+};
+
+export default ServicesPage;
---
+++ app/dashboard/api/page.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const apiPage: NextPage = () => {
+  return (
+    <div>
+      <h1>Api Page</h1>
+    </div>
+  );
+};
+
+export default apiPage;
